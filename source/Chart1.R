e7e2b2da8f55878919deed1cc97f05a522e2b583
elm53 <114891144+elm53@users.noreply.github.com>
2022-12-07 16:48:02 -0800

    chart

--- source/Chart1.R
+++ source/Chart1.R
@@ -116,6 +116,7 @@ plot_date_start <- function(dates) {
   return(plot_start)
 }
 
+# Work for interactive plot, it won't show up in shiny but this graph works
 interactive_plot <- function(dates) {
   covid_plotly <- plot_ly(get_date_start(dates), x = ~Date, y = ~Frequency, type = "scatter", mode = "lines") %>%
     layout(title = paste("Crime in Washington from", dates, "to 2022-10-25"), yaxis = list(title = "Number of Crimes a Day"))
